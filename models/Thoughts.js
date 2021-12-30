68d5330028a14e541c3d48b6a447d00e2a12ebd2
Gerrit Elenbaas <84500222+Gelenhaus@users.noreply.github.com>
2021-12-30 16:20:46 -0700

    I am now getting some of the thought routes to work

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,6 +1,31 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+
+
+const ReactionSchema = new Schema({
+
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => newTypes.ObjectId()
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    }
+
+});
+
 const ThoughtsSchema = new Schema({
     thoughtText: {
         type: String,
@@ -14,11 +39,27 @@ const ThoughtsSchema = new Schema({
         require: true,
         trim: true
     },
+
     createdAt: {
         type: Date,
         default: Date.now,
         get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    reactions: [ReactionSchema]
+
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
+
+);
+
+ThoughtsSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
 });
 
 const Thoughts = model('Thoughts', ThoughtsSchema);
